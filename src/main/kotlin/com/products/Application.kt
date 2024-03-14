b704b4097f70bc7efc86c87a43f70f6f27a75347
arjeshvadadoriya4052 <arjesh.vadadoriya@grewon.com>
2024-03-14 14:25:19 +0530

    first commit

---
+++ src/main/kotlin/com/products/Application.kt
@@ -0,0 +1,12 @@
+package com.products
+
+import com.products.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+}
